ccfe05589ebdffd35319fed5b16484c11d9d8c3f
binhetech <binhetech@163.com>
2020-06-15 18:35:38 +0800

    imbalance cls

--- scripts/05-sed_train.sh
+++ scripts/05-sed_train.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export BERT_BASE_DIR=../models/cased_L-24_H-1024_A-16
+export BERT_BASE_DIR=../models/uncased_L-12_H-768_A-12
 export DATA_DIR=../glue
 
 nohup python3 ../run_classifier.py \
@@ -11,8 +11,8 @@ nohup python3 ../run_classifier.py \
   --vocab_file=$BERT_BASE_DIR/vocab.txt \
   --bert_config_file=$BERT_BASE_DIR/bert_config.json \
   --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
-  --max_seq_length=512 \
-  --train_batch_size=64 \
+  --max_seq_length=128 \
+  --train_batch_size=32 \
   --learning_rate=2e-5 \
   --num_train_epochs=10 \
   --num_gpu_cores=1 \
--- scripts/103-garbledSents_train.sh
+++ scripts/103-garbledSents_train.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-export BERT_BASE_DIR=../models/cased_L-12_H-768_A-12
+export BERT_BASE_DIR=../models/uncased_L-12_H-768_A-12
 export DATA_DIR=../glue
 
-python3 ../run_classifier.py \
+python3 ../run_classifier_imbalance.py \
   --task_name=garbledSents \
-  --do_train=False \
+  --do_train=True \
   --do_eval=True \
   --do_predict=False \
   --data_dir=$DATA_DIR/garbledSents \
@@ -16,9 +16,9 @@ python3 ../run_classifier.py \
   --train_batch_size=32 \
   --eval_batch_size=64 \
   --predict_batch_size=64 \
-  --learning_rate=1e-5 \
-  --num_train_epochs=12 \
+  --learning_rate=2e-5 \
+  --num_train_epochs=5 \
   --num_gpu_cores=2 \
-  --do_lower_case=False \
-  --output_dir=../fine_tuned/garbledSents \
+  --do_lower_case=True \
+  --output_dir=../fine_tuned/garbledSents-uncased \
   | tee garbledSents.log
